fc7e14b8b9d2a7f24233cad3c4112ac35b778f84
Veeresh S A <95859687+Veeresh-1999@users.noreply.github.com>
2024-07-24 18:12:12 +0000

    type converstion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,49 @@
+/* REMOVE THE COMMENT AND TRY */
+// let score = "true"
+// console.log(typeof(score));
+// let valueInNumber = Number(score)
+// console.log(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+/*
+    "33"      -> 33
+    "33abc"   -> NaN (Not a number)
+    true      -> 1
+    false     -> 0
+    "veeresh" -> NaN
+*/
+
+/*-------------------------------------------------------------------------------------------------*/
+/* REMOVE THE COMMENT AND TRY */
+// let isLoggedIn = ""
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof(booleanIsLoggedIn));
+// console.log(booleanIsLoggedIn);
+
+/*
+1         -> true
+0         -> false
+"veeresh" -> true
+""        -> false
+*/
+
+/*-------------------------------------------------------------------------------------------------*/
+/* REMOVE THE COMMENT AND TRY */
+let someNumber = true
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
+/*
+    33      -> string
+    "33abc" -> string
+    true    -> string
+    false   -> string
+    ""      -> string
+    " "     -> string
+*/
+
+
+
